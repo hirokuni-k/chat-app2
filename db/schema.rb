8398cacc8753e8a9f3e07c19174379db7b1ea683
hirokuni-k <shisenramen@yahoo.co.jp>
2022-01-27 00:57:19 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_081849) do
+ActiveRecord::Schema.define(version: 2022_01_26_155431) do
+
+  create_table "rooms", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
